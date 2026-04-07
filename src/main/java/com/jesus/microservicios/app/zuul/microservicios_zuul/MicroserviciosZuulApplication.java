eb18a78cee227d4dfdc1b8ab8bbeed4742cebab7
Jesusmslformacion <chechuelcrack@gmail.com>
2026-04-07 11:45:03 +0200

    Agregar archivos de configuración y estructura inicial para el microservicio Zuul

---
+++ src/main/java/com/jesus/microservicios/app/zuul/microservicios_zuul/MicroserviciosZuulApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.microservicios.app.zuul.microservicios_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosZuulApplication.class, args);
+	}
+
+}
